fd347e0275a5c50bd828557dd24c0e7b33e686cb
chzhonge <chzhonge@gmail.com>
2026-06-11 11:46:10 +0800

    chore: revert monkey patch for fabric.js textBaseline warning

--- src/main.ts
+++ src/main.ts
@@ -12,33 +12,6 @@ import { VueMasonryPlugin } from 'vue-masonry';
 
 import i18n from './language/index';
 
-import { fabric } from 'fabric';
-
-// 修復 fabric.js v5.x 版本的 'alphabetical' TextBaseline 錯誤警告
-if (fabric && fabric.Text && fabric.Text.prototype) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  fabric.Text.prototype._setTextStyles = function (ctx: any, charStyle: any, forMeasuring: any) {
-    ctx.textBaseline = 'alphabetic'; // 原本的原始碼寫錯成 'alphabetical'，這裡強制作修正
-    if (this.path) {
-      switch (this.pathAlign) {
-        case 'center':
-          ctx.textBaseline = 'middle';
-          break;
-        case 'ascender':
-          ctx.textBaseline = 'top';
-          break;
-        case 'descender':
-          ctx.textBaseline = 'bottom';
-          break;
-      }
-    }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    ctx.font = this._getFontDeclaration(charStyle, forMeasuring);
-  };
-}
-
 async function bootstrap() {
   const app = createApp(App);
   app.use(VueMasonryPlugin);
